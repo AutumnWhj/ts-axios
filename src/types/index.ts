06f59e7fb7e2af0ae440664070df59dbccc9b10b
Autumn <Autumn@wanghuajindeMacBook-Pro.local>
2022-02-26 11:05:45 +0800

    feat(axios): axios函数重载

--- src/types/index.ts
+++ src/types/index.ts
@@ -61,4 +61,6 @@ export interface Axios {
 
 export interface AxiosInstance extends Axios {
   (config: AxiosRequestConfig): AxiosPromise
+
+  (url: string, config?: AxiosRequestConfig): AxiosPromise
 }
