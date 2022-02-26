7dbbef0b3ece69f871c936760592bf96a84679df
Autumn <Autumn@wanghuajindeMacBook-Pro.local>
2022-02-26 09:37:27 +0800

    feat(axios): axios data数据处理 json化

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 import { AxiosRequestConfig } from './types'
 function axios(config: AxiosRequestConfig) {
   processConfig(config)
@@ -8,10 +9,15 @@ function axios(config: AxiosRequestConfig) {
 
 function processConfig(config: AxiosRequestConfig) {
   config.url = transformUrl(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig) {
   const { url, params } = config || {}
   return buildURL(url, params)
+}
+function transformRequestData(config: AxiosRequestConfig) {
+  const { data } = config || {}
+  return transformRequest(data)
 }
 export default axios
